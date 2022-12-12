8474226025068b427720a16229fe443cbcad520a
Annette Bwemere <bwemereannette@gmail.com>
2022-12-12 19:36:00 +0200

    stylebutoon

--- js/index.js
+++ js/index.js
@@ -10,9 +10,10 @@ const timer = {
 let interval;
 
 
-
+const buttonSound = new Audio('button-sound.mp3')
 const mainButton = document.getElementById('js-btn');
 mainButton.addEventListener('click', ()=>{
+    buttonSound.play();
     const {action} = mainButton.dataset;
 
     if (action === "start") {
@@ -23,51 +24,10 @@ mainButton.addEventListener('click', ()=>{
 })
 
 
-function stopTimer() {
-    clearInterval(interval);
-
-    mainButton.dataset.action = 'start'
-    mainButton.textContent = 'start'
-    mainButton.classList.remove('active')
-}
-
-function handleMode(e) {
-    const {mode} = e.target.dataset;
-
-
-    if (!mode) return;
-    
-    switchMode(mode);
-    stopTimer();
-}
-
 
 
 const modeButtons = document.querySelector('#js-mode-buttons');
 modeButtons.addEventListener('click', handleMode);
-
-
-
-function getRemainingTime(endTime) {
-    
-    const currentTime = Date.parse(new Date());
-    const difference = endTime - currentTime;
-
-
-
-    const total = Number.parseInt(difference / 1000, 10);
-    const minutes = Number.parseInt((total/60) % 60, 10);
-    const seconds = Number.parseInt(total % 60, 10);
-
-
-
-    return {
-
-        total,
-        minutes,
-        seconds
-    };
-}
 
 
 
@@ -77,10 +37,9 @@ function startTimer() {
 
     if (timer.mode === 'pomodoro') timer.sessions++;    
 
-mainButton.dataset.action = 'stop';
-mainButton.textContent= 'stop'
-mainButton.classList.add('active')
-
+    mainButton.dataset.action = 'stop';
+    mainButton.textContent= 'stop'
+    mainButton.classList.add('active')
 
     interval = setInterval(()=>{
         timer.remainingTime = getRemainingTime(endTime);
@@ -103,6 +62,8 @@ mainButton.classList.add('active')
                 switchMode('pomodoro');
         }
 
+        document.querySelector(`[data-sound="${timer.mode}"]`).play();
+
         startTimer()
         }
     }, 1000);
@@ -110,30 +71,37 @@ mainButton.classList.add('active')
 
 
 
+//stoperTimer Function
 
-function switchMode(mode) {
-    timer.mode = mode;
-    timer.remainingTime = {
-        total: timer[mode] * 60,
-        minutes: timer[mode],
-        seconds: 0,
-    }
+function stopTimer() {
+    clearInterval(interval);
 
-
-    document
-        .querySelectorAll('button[data-mode]')
-        .forEach(e=>e.classList.remove('active'));
-    document
-        .querySelector(`[data-mode="${mode}"]`).classList.add('active');
-      document
-        .body.style.backgroundImage = `var(--${mode})`;
-// console.log("Annette Switch");
-document
-    .getElementById('js-progress')
-    .setAttribute('max', timer.remainingTime.total);
-      
-updateClock();
+    mainButton.dataset.action = 'start'
+    mainButton.textContent = 'start'
+    mainButton.classList.remove('active')
 }
+
+
+
+function getRemainingTime(endTime) {
+    
+    const currentTime = Date.parse(new Date());
+    const difference = endTime - currentTime;
+
+    const total = Number.parseInt(difference / 1000, 10);
+    const minutes = Number.parseInt((total/60) % 60, 10);
+    const seconds = Number.parseInt(total % 60, 10);
+
+
+    return {
+
+        total,
+        minutes,
+        seconds
+    };
+}
+
+
 
 
 
@@ -143,7 +111,6 @@ function updateClock() {
 
     const minutes = `${remainingTime.minutes}`.padStart(2, '0');
     const seconds = `${remainingTime.seconds}`.padStart(2, '0');
-
 
 
     const min = document.getElementById('js-minutes');
@@ -160,6 +127,44 @@ function updateClock() {
     progress.value = timer[timer.mode] * 60 - timer.remainingTime.total
 }
 
+
+
+
+
+function switchMode(mode) {
+    timer.mode = mode;
+    timer.remainingTime = {
+        total: timer[mode] * 60,
+        minutes: timer[mode],
+        seconds: 0,
+    }
+
+    document
+        .querySelectorAll('button[data-mode]')
+        .forEach(e=>e.classList.remove('active'));
+    document
+        .querySelector(`[data-mode="${mode}"]`).classList.add('active');
+      document
+        .body.style.backgroundImage = `var(--${mode})`;
+// console.log("Annette Switch");
+    document
+    .getElementById('js-progress')
+    .setAttribute('max', timer.remainingTime.total);
+      
+updateClock();
+}
+
+
+
+function handleMode(e) {
+    const {mode} = e.target.dataset;
+
+
+    if (!mode) return;
+    
+    switchMode(mode);
+    stopTimer();
+}
 
 
 
